7fcc8e163adca16eb270241e3d2bc792fef2b703
deyvissoneduardo <deyvissonarouche@gmail.com>
2023-01-29 19:01:42 -0300

    splah

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:vakinha_burguer_mobile/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:get/utils.dart';
 import 'package:vakinha_burguer_mobile/app/core/ui/widgets/vakinha_button.dart';
-import 'package:vakinha_burguer_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,20 +8,37 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: VakinhaAppbar(),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          const Padding(
-            padding: EdgeInsets.all(10.0),
-            child: VakinhaTextformfield(label: 'Text'),
-          ),
-          const SizedBox(height: 20),
-          VakinhaButton(
-            label: 'Butron',
-            onpressed: () {},
-          ),
-        ],
+      body: Container(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.width,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(height: context.heightTransformer(reducedBy: 85)),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 60),
+                  VakinhaButton(
+                    label: 'ACESSAR',
+                    width: context.widthTransformer(reducedBy: 25),
+                    onpressed: () {},
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
